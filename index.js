216e3da57ec7fc32a240474668e7e00417732179
Immortal-codes <rajanraj8979@gmail.com>
2021-12-18 17:18:54 +0530

    laksjdksa

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080;
 
 // sendFile will go here
 
-app.use(express.static(__dirname, '/dist'));
+app.use(express.static(__dirname, '/dist/'));
 
 //app.get('/', function(req, res) {
 //  res.sendFile(path.join(__dirname, '/dist/index.html'));
