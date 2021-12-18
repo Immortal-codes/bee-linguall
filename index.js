9fdea0c37fe2b318f5d41095a227c554bf9afd72
Immortal-codes <rajanraj8979@gmail.com>
2021-12-18 17:25:50 +0530

    asdasdasd

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path')
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -7,13 +8,13 @@ const port = process.env.PORT || 8080;
 
 //app.use(express.static(__dirname, '/dist/'));
 
-//app.get('/', function(req, res) {
-//  res.sendFile(path.join(__dirname, '/dist/index.html'));
-//});
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
-app.get('/', (req,res)=>{
-   res.send("Hello world");
-})
+//app.get('/', (req,res)=>{
+//   res.send("Hello world");
+//})
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
