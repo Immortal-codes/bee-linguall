453f71f4981e48e27fa70d990a5403fbf7b06e27
Immortal-codes <rajanraj8979@gmail.com>
2021-12-18 17:16:33 +0530

    commit ajsd

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname, '/dist'));
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/dist/index.html'));
-});
+//app.get('/', function(req, res) {
+//  res.sendFile(path.join(__dirname, '/dist/index.html'));
+//});
+
+app.get('/', (req,res){
+   res.send("Hello world");
+})
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
