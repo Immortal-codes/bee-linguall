f1c7755b7e0aa140afe81f3d2f6849faf645da46
Immortal-codes <rajanraj8979@gmail.com>
2021-12-18 17:09:33 +0530

    third commit

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
+
+app.use(express.static(__dirname, '/dist'));
+
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
 });
 
 app.listen(port);
